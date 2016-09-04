c2d970d4da480eb397b1f06a9f4f73246a0ab4a4
Evelina Pancheva <eveto@Evelina.local>
2016-09-04 17:23:56 +0200

    Single book ready; fixed request issues; Others

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
 var googleBooks = angular.module('googleBooks',[
-	'ngRoute',
 	'booksControllers',
 	'infinite-scroll'
 ]);
-
-googleBooks.config(['$routeProvider', function($routeProvider){
-	$routeProvider.
-	when('/',{
-		templateUrl: 'views/searchResults.html',
-		controller: 'SearchController'
-	}).
-	when('/book-details/:itemId',{
-		templateUrl: 'views/book.html',
-		controller: 'SingleBookController'
-	}).
-	otherwise({
-		redirectTo: '/'
-	});
-}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,45 +1,54 @@
 var booksControllers = angular.module('booksControllers',[]);
 
-booksControllers.controller('SearchController',['$scope','$http','$routeParams', function($scope,$http,$routeParams) {
+booksControllers.controller('SearchController',['$scope','$http', function($scope,$http) {
+	var apiKey = "AIzaSyCTwJYPf7-DpuD2C0OfQFKExXhqWPs7Wts";
+	$scope.hideSingleBook = function (){
+		$scope.showSingleBook = false;
+	}
 	$scope.searchIn = "whatever";
+	$scope.hideSingleBook();
 
-	$scope.getMeSomeExtraBooks = function (){
-		 if (typeof $scope.booksResults == "undefined") return;
+	$scope.getScrollLikeTheWind = function (){
+		if (typeof $scope.booksResults == "undefined" || $scope.startIndex > 500) return;
 	    $scope.getMeSomeBooks($scope.query+'&startIndex='+$scope.startIndex);
 	    $scope.startIndex +=10;
-	    console.log("1");
 	}
 
 	$scope.getMeSomeBooks = function (url){
 		$http.get(url)
-		.then(function(data){
-			$scope.booksResults = $scope.booksResults.length ? $scope.booksResults.concat(data.data.items) : data.data.items;
+		.then(function itWentWell (data){
+			if(data.data.totalItems == 0) {
+				$scope.emptyResponse = true;
+				return;
+			} else {
+				$scope.emptyResponse = false;
+			}
+			$scope.booksResults = $scope.booksResults ? $scope.booksResults.concat(data.data.items) : data.data.items;
+		},function itDidntGoWell (data) {
+			$scope.requestError = data.data.error.message;
 		});
 	}
 	$scope.whatToSearchFor = function (){
 		if($scope.searchInput.length > 3) {
 			$scope.query = 'https://www.googleapis.com/books/v1/volumes?q='+$scope.searchInput;
-			$scope.booksResults = [];
+			$scope.booksResults = $scope.booksResults ? [] : undefined;
 			switch ($scope.searchIn) {
 				case "inauthor":
 				case "intitle":
-					$scope.query+= "+"+$scope.searchIn+":"+$scope.searchInput+'&maxResults=10';
+					$scope.query+= "+"+$scope.searchIn+":"+$scope.searchInput+'&maxResults=10&key='+apiKey;
 					$scope.getMeSomeBooks($scope.query);
 					break;
 				case "whatever":
-					$scope.query +='&maxResults=10';
+					$scope.query +='&maxResults=10&key='+apiKey;
 					$scope.getMeSomeBooks($scope.query);
-
 			}
 		}
 		$scope.startIndex = 10;
 	}
-}]);
 
-booksControllers.controller('SingleBookController',['$scope','$http','$routeParams', function($scope,$http,$routeParams) {
-	$scope.bookId = $routeParams.itemId;
-	$http.get('https://www.googleapis.com/books/v1/volumes/'+$scope.bookId)
-	.then(function(data){
-		$scope.bookInfo = data.data;
-	});
+	$scope.loadMoreInfo = function (index) {
+		$scope.specificBook = $scope.booksResults[index];
+		$scope.showSingleBook = true;
+	}
+
 }]);
